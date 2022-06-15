7256b4764fd5ea6aac2c9d74dfb3fff3f2c8607f
SOHAN-cyber <sohandogra703@gmail.com>
2022-06-15 20:37:04 +0530

    initial

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -15,11 +15,4 @@ pipelineJob('INFRA/SingleNetworkSetupJob') {
             scriptPath('Jenkinsfile')
         }
     }
-     parameters {
-          booleanParam(
-            defaultValue: true, 
-            description: 'Do you want to create Network', 
-            name: 'Network'
-          )
-     }
 }
